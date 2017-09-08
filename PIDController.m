b41d8e9ec3b04b975730d1da1c6a6371e5fad5c1
jschlind411 <jschlind411@github.com>
2017-09-07 18:00:53 -0600

    made minor fix/cleaned code... need to finish div and int terms

--- PIDController.m
+++ PIDController.m
@@ -2,6 +2,7 @@
 
 function control_forces = PIDController( current_time, joint1_angle_setpoint, joint2_angle_setpoint, joint1_measured_angle, joint2_measured_angle, error1_dot, error2_dot )
 
+%LAST KNOWN ERRORS AND TIME
 persistent lastError1; 
 persistent lastError2;
 persistent lastTime;
@@ -9,65 +10,67 @@ persistent lastTime;
 torque1 = 0;
 torque2 = 0;
 
+%If first time running through PID
 if(isempty(lastError1) && isempty(lastError2) && isempty(lastTime))
     lastError1 = 0;
     lastError2 = 0;
     lastTime = 0.01;
 end
 
+%instantiate constants
 kp = 1;
 kd = 1;
 ki = 1;
 
-%deltaTime
-dTime = current_time - lastTime
+%Find Delta Time
+dTime = current_time - lastTime;
 
+%Instantiate known steps
 propTerm1 = 0;
 propTerm2 = 0;
 divTerm1 = 0;
-errorTime2 = 0;
+divTirm2 = 0;
 f1 = 0;
 f2 = 0;
    
 % f(x) = Kp(desiredYaw - actualYaw) + Kd(deltaError/deltaTime) + integral[(errordt)Ki]
-   
 %-----------------------------------------------------------
 
 %JOINT1
-   
+%-----------------------------------------------------------
 %Proportional  Term:    
-propTerm1 = (joint1_angle_setpoint - joint1_measured_angle);
+propTerm1 = (joint1_angle_setpoint - joint1_measured_angle);    %Working.... ish
 %Derivitive Term:
-divTerm1 = (propTerm1 -lastError1) / (dTime);
+divTerm1 = (propTerm1 -lastError1) / (dTime);                   %?
 %Integral Term:
-intTerm1 = 0;  %working on calculation
+intTerm1 = 0;                                                   %working on calculation
+
+torque1 = (kp * propTerm1) + (kd * divTerm1)% + (ki * f1)    %calculate
 
 %-----------------------------------------------------------
 
 %JOINT2
+%-----------------------------------------------------------
 %Proportional  Term:    
-propTerm2 = (joint2_angle_setpoint - joint2_measured_angle);
+propTerm2 = (joint2_angle_setpoint - joint2_measured_angle);    %Working... ish?
 %Derivitive Term:
-divTerm1 = (propTerm2 -lastError2) / (dTime);
+divTerm2 = (propTerm2 -lastError2) / (dTime);                   %?
 %Integral Term:
-intTerm2 = 0;  %working on calculation
+intTerm2 = 0;                                                   %working on calculation
+
+torque2 = 0 % (kp * propTerm2)% + (kd * errorTime2)% + (ki * f2) 
+
+%-----------------------------------------------------------
 
 %integral(fun,0,1)
 
-%Calculate Torque & Fill Vector
-%-----------------------------------------------------------
-% torque1, torque2 paired with joint1, joint2
-torque1 = (kp * propTerm1)% + (kd * errorTime1)% + (ki * f1)
-torque2 = 0 % (kp * propTerm2)% + (kd * errorTime2)% + (ki * f2) 
-
-
-
+%Store in vector
 control_forces = [torque1; torque2];
 
 %Setup function for next calculation
 %-----------------------------------------------------------
-lastError1 = propTerm1
-lastError2 = propTerm2
-lastTime = current_time
+lastError1 = propTerm1;
+lastError2 = propTerm2;
+lastTime = current_time;
 
 end
