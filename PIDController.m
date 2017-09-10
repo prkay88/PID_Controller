11db13ae86a98d19587ed848c78bf9be49988a58
Aakash <Aakash@10.87.89.106>
2017-09-10 01:21:32 -0600

    The integral term is added and also the total integral is calculated by adding the previous total integral with the integral term. Total integral us used to tune the PID controller. The derivative term is also changed and the derivative term is just error1_dot and error2_dot which is passed in the function argument. Also, our own derivative term is calculated but it is commented because it throws alot of warning when the code with the derived derivative term is run. Need to fix this issue and get the correct derivative term All the code is cleaned up. Matrix is used for doing the calculation for both joint at once, instead of doing it two times.

--- PIDController.m
+++ PIDController.m
@@ -2,75 +2,50 @@
 
 function control_forces = PIDController( current_time, joint1_angle_setpoint, joint2_angle_setpoint, joint1_measured_angle, joint2_measured_angle, error1_dot, error2_dot )
 
-%LAST KNOWN ERRORS AND TIME
-persistent lastError1; 
-persistent lastError2;
+%% LAST KNOWN ERRORS AND TIME 
+persistent lastError;
 persistent lastTime;
+persistent totalIntegral;
 
-torque1 = 0;
-torque2 = 0;
 
-%If first time running through PID
-if(isempty(lastError1) && isempty(lastError2) && isempty(lastTime))
-    lastError1 = 0;
-    lastError2 = 0;
+%% If first time running through PID
+if(isempty(lastTime) && isempty(lastError))
+    lastError = [0;0];
     lastTime = 0.01;
+    totalIntegral = [0;0];
 end
 
-%instantiate constants
+%% Instantiate constants
 kp = 1;
 kd = 1;
 ki = 1;
 
-%Find Delta Time
+
+%% CALCULATING PID FOR BOTH JOINTS
+% f(x) = Kp(desiredYaw - actualYaw) + Kd(deltaError/deltaTime) + integral[(errordt)Ki]
+
 dTime = current_time - lastTime;
 
-%Instantiate known steps
-propTerm1 = 0;
-propTerm2 = 0;
-divTerm1 = 0;
-divTirm2 = 0;
-f1 = 0;
-f2 = 0;
-   
-% f(x) = Kp(desiredYaw - actualYaw) + Kd(deltaError/deltaTime) + integral[(errordt)Ki]
-%-----------------------------------------------------------
+propTerm = [joint1_angle_setpoint - joint1_measured_angle ;
+            joint2_angle_setpoint - joint2_measured_angle ];
+devTerm = (propTerm - lastError)./ dTime;        
+intTerm = dTime .* propTerm;
+ 
+ totalIntegral = totalIntegral + intTerm;
+ 
+ % calculating by using the derivative term given from the parameter of the
+ % function
+ control_forces = kp .* propTerm + kd .* [error1_dot;error2_dot] + ki .* totalIntegral;
+ 
+ % Calculating by using the the derivative term that we calacuated on our
+ % own. To get more than 93/100 in this assignment we are supposed to
+ % create a derivative term on our own. 
+ % Formula for control force using our own derivative term is : 
+ % control_forces = kp .* propTerm + kd .* devTerm + ki .* totalIntegral;
 
-%JOINT1
-%-----------------------------------------------------------
-%Proportional  Term:    
-propTerm1 = (joint1_angle_setpoint - joint1_measured_angle);    %Working.... ish
-%Derivitive Term:
-divTerm1 = (propTerm1 -lastError1) / (dTime);                   %?
-%Integral Term:
-intTerm1 = 0;                                                   %working on calculation
 
-torque1 = (kp * propTerm1) + (kd * divTerm1)% + (ki * f1)    %calculate
-
-%-----------------------------------------------------------
-
-%JOINT2
-%-----------------------------------------------------------
-%Proportional  Term:    
-propTerm2 = (joint2_angle_setpoint - joint2_measured_angle);    %Working... ish?
-%Derivitive Term:
-divTerm2 = (propTerm2 -lastError2) / (dTime);                   %?
-%Integral Term:
-intTerm2 = 0;                                                   %working on calculation
-
-torque2 = 0 % (kp * propTerm2)% + (kd * errorTime2)% + (ki * f2) 
-
-%-----------------------------------------------------------
-
-%integral(fun,0,1)
-
-%Store in vector
-control_forces = [torque1; torque2];
-
-%Setup function for next calculation
-%-----------------------------------------------------------
-lastError1 = propTerm1;
-lastError2 = propTerm2;
+%% Setup function for next calculation
+lastError = propTerm;
 lastTime = current_time;
 
 end
